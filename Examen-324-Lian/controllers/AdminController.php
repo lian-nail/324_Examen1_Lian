51a9bd92bc8886c7adc6637f2a9fe9cb991523c2
lian-nail <113316699+lian-nail@users.noreply.github.com>
2024-05-06 17:53:15 -0400

    Update AdminController.php

--- Examen-324-Lian/controllers/AdminController.php
+++ Examen-324-Lian/controllers/AdminController.php
@@ -133,34 +133,19 @@ class AdminController {
             'alertas' => $alertas
         ]);
     }
+    public static function eliminarUs() {
+        session_start();
+        isAdmin();
+        
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+            
+            $id = $_POST['id'];
+            $persona = Persona::find($id);
+    
+            $persona->eliminar();
+    
+            header('Location: /admin/usuarios');
+        }
+    } 
 }
 
-
-
-// $fecha = $_GET['fecha'] ?? date('Y-m-d');
-// $verificarFecha = explode('-', $fecha);
-
-// if( !checkdate($verificarFecha[1], $verificarFecha[2], $verificarFecha[0]) ) {
-//     header('Location: /404');
-// }
-
-// //Consultar la base de datos
-// $consulta = "SELECT citas.id, citas.hora, CONCAT( usuarios.nombre, ' ', usuarios.apellido) as cliente, ";
-// $consulta .= " usuarios.email, usuarios.telefono, servicios.nombre as servicio, servicios.precio  ";
-// $consulta .= " FROM citas  ";
-// $consulta .= " LEFT OUTER JOIN usuarios ";
-// $consulta .= " ON citas.usuarioId=usuarios.id  ";
-// $consulta .= " LEFT OUTER JOIN citasServicios ";
-// $consulta .= " ON citasServicios.citaId=citas.id ";
-// $consulta .= " LEFT OUTER JOIN servicios ";
-// $consulta .= " ON servicios.id=citasServicios.servicioId ";
-// $consulta .= " WHERE fecha =  '${fecha}' ";
-
-// $citas = AdminCita::SQL($consulta);
-
-
-// $router->render('admin/index', [
-//     'nombre' => $_SESSION['nombre'],
-//     'citas' => $citas,
-//     'fecha' => $fecha
-// ]);
